acd212e6f7e344192152213abc43122095088435
Diego Alejandro Campos <dcamposm@unal.edu.co>
2024-03-19 18:30:30 +0000

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,19 +9,19 @@ int main(int argc, char const *argv[])
     std::vector<Molecule> ini_mol(N_mol, Molecule(pos_ini(10))); // vector que contiene las clases Molecule
 
     // for (int ii = 0; ii < N_mol; ++ii){ 
-    //     std::cout << ini_mol[ii].position[0];
+    //     std::cout << ini_mol[ii].position[0] << std::endl;
     //     // Molecule mol({0,0}); // Se crea una molecula con posición inicial aleatoria
     //     // ini_mol.push_back(Molecule(pos_ini(10))); // Se agrega la molecula al vector
     // }
 
 
     Cream cup(ini_mol);
-    // std::cout << cup.molecules[2].position[0] << std::endl;
+    // std::cout << cup.molecules[0].position()[0] << std::endl;
 
-    // for (int ii = 0; ii < N_mol; ++ii){ 
-    //     int pos_time = cup.molecules[ii].position[0];
-    //     // std::cout << pos_time[0] << std::endl;
-    // }
+    for (int ii = 0; ii < N_mol; ++ii){ 
+        int pos_time = cup.molecules[ii].position[0];
+        std::cout << pos_time << std::endl;
+    }
 
-    // return 0;
+    return 0;
 }
