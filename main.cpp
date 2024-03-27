ddad5f820680440bf713beb76743e968d71d1689
svargasro <svargasro@unal.edu.co>
2024-03-26 20:31:03 -0500

    Input.txt added

--- main.cpp
+++ main.cpp
@@ -1,14 +1,41 @@
 #include "cream.h"
 #include "random69.h"
 #include <iostream>
-
+#include <fstream>
+#include <string>
+#include <typeinfo>
 
 int main(int argc, char const *argv[])
 {
-    int N_mol = 400; //Number of molecules
-    int n_iterations = 10; //Niterations
-    int latticesize = 100; //latticesize
-    int seed = 0; //randomgeneratorsedd
+
+std::vector<int> input(4); //Vector que guarda los parámetros de entrada.
+
+std::fstream inputFile;
+inputFile.open("input.txt", std::ios::in); //Abre el archivo en modo lectura.
+
+ if (inputFile.is_open()){  //Se verifica que esté abierto.
+     std::string line;
+     int inputContador = 0;
+      while(getline(inputFile, line)){  //Se lee cada línea del archivo.
+          int pos = line.find(' '); // Busca el primer espacio en la línea
+          std::string numberLine = line.substr(0, pos); // Extrae la parte de la cadena antes del espacio
+
+        int val=std::stoi(numberLine); //Se convierten valores numéricos a enteros.
+        input[inputContador] = val;
+        inputContador++;
+      }
+      inputFile.close();   //close the file object.
+ }
+
+
+
+
+
+
+    int N_mol = input[0]; //Number of molecules (400)
+    int n_iterations = input[1]; //Niterations (20)
+    int latticesize = input[2]; //latticesize (10)
+    int seed = input[3]; //randomgeneratorsedd (0)
 
 
     std::vector<Molecule> molecules(N_mol);
