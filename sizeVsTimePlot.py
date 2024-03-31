b676b3228cf793446becf0e396c8408d7b6570d1
Diego Alejandro Campos <dcamposm@unal.edu.co>
2024-03-31 04:29:25 +0000

    fix size plot

--- sizeVsTimePlot.py
+++ sizeVsTimePlot.py
@@ -11,29 +11,29 @@ def lineal_model(x, a, b):
 t, size= np.genfromtxt('SizeVsTime.txt', unpack=True, usecols=(0, 1))
 
 # Ajuste
-parameters, covarian_matrix = curve_fit(lineal_model, t, size)
+parameters, covarian_matrix = curve_fit(lineal_model, t[:2*10**3], size[:2*10**3])
 a, b = parameters
 
-t_fit = np.linspace(t[0], t[-1], 100)
+t_fit = t[:3*10**3]
 
 
-sizeAdjusted = lineal_model(t, a, b)
+sizeAdjusted = lineal_model(t_fit, a, b)
 
-r2, _ = pearsonr(size, sizeAdjusted)
+r2, _ = pearsonr(size[:3*10**3], sizeAdjusted)
 
 
 fig, axes = plt.subplots(figsize=(6, 6))
 
 
-axes.scatter(t, size, marker='.', color='black', label= 'Size(t)')
+axes.scatter(t, size, s=4, color='black', label= 'Size(t)')
 
-axes.plot(t, sizeAdjusted, color='red',label=r'{} $t^{{1/2}}$ + {}  $R^2$ ={}'.format(round(b,2),round(a,3),round(r2,4)))
+axes.plot(t_fit, sizeAdjusted, color='red',label=r'{} $t$ + {} ($R^2$ ={})'.format(round(b,2),round(a,3),round(r2,4)))
 
 # Se ajustan demás detalles del gráfico.
 
-axes.set_xlabel('t', fontsize=12)
-axes.set_ylabel('Size', fontsize=12)
-axes.set_title("Size of the drop  Vs t")
+axes.set_xlabel('Time', fontsize=12)
+axes.set_ylabel(f'$(\sum r^2_i/ N)$', fontsize=12)
+axes.set_title("Squared size of the drop in function of time")
 axes.legend()
 axes.grid(True, linestyle='--')
 fig.savefig('sizeVsTimePlot.png')
