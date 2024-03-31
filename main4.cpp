614ced8e05e0bafd93e340a45cdc7714a55d2b4e
jcastromi <91992054+jcastromi@users.noreply.github.com>
2024-03-30 21:08:00 -0500

    Cambios para recibir del archivo

--- main4.cpp
+++ main4.cpp
@@ -2,20 +2,43 @@
 #include "mol.cpp"
 
 int main(int argc, char *argv[]) {
-    //std::ifstream myfile ("imput.txt");
-    //std::string line;
-    //myfile.is_open();
-    //std::vector<int> dat(4);
-    //getline(myfile,line);
-    int dim=50;
-    int mol=50;
-    int SEED=0;
-    int niter=300000;
-    std::vector<std::vector<int>> Molecules(mol,std::vector<int> (2));
-    Molecules=POSS(dim,mol,Molecules);
+
+    std::vector<int> input(4); //Vector que guarda los parámetros de entrada.
+    std::fstream inputFile;
+    inputFile.open("input.txt", std::fstream::in); //Abre el archivo en modo lectura.
+
+    if (inputFile.is_open()){  //Se verifica que esté abierto.
+        std::string line;
+        int inputContador = 0;
+        while(getline(inputFile, line)){  //Se lee cada línea del archivo.
+            int pos = line.find(' '); // Busca el primer espacio en la línea
+            std::string numberLine = line.substr(0, pos); // Extrae la parte de la cadena antes del espacio
+
+            int val=std::stoi(numberLine); //Se convierten valores numéricos a enteros.
+            input[inputContador] = val;
+            inputContador++;
+        }
+        inputFile.close();   //close the file object.
+    }
+
+
+    const int N_mol = input[0]; //Number of molecules
+    const int latticesize = input[1]; //latticesize
+    const int niter = input[2]; //Niterations
+    const int SEED= input[3]; //randomgeneratorsedd
+
+    std::vector<std::vector<int>> Molecules(N_mol,std::vector<int> (2));
+    Molecules=POSS(latticesize,N_mol,Molecules);
+    std::mt19937 gen(SEED);
+    std::vector<int> Prob (2);
+    std::uniform_int_distribution<> distrib(1,4);
+    std::uniform_int_distribution<> dis(0,N_mol-1);
+
 
     for(int ju=0; ju<niter;ju++){
-        Molecules=MOVE(step(SEED,mol),Molecules, dim);
+        Prob[0]=distrib(gen);
+        Prob[1]=dis(gen);
+        Molecules=MOVE(Prob,Molecules, latticesize);
         std::cout<<ju<<"\t"<<SIZ(Molecules)<<'\n';
     }
     return EXIT_SUCCESS;
